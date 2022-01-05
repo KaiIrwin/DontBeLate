f2946b9545ed6e4b18e6c54ada51ae7a3a2d2eb9
Kai Irwin <kaiirwin@KainoMacBook-Air.local>
2022-01-05 15:39:33 +0900

    一応 resultOfTheDeal の変数を .erb file にも使えるように設定した。しかし自動的に request しないので困る

--- httparty.rb
+++ httparty.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 
 # Auth key updates. You need to change the Auth key once in a while and you need to autimate that
-presenceOfUserFullAPIResponse = HTTParty.get('https://slack.com/api/users.getPresence', headers: {"Authorization" => "Bearer xoxp-2897666266758-2928076043648-2906416966645-6238ebabc164b6b6ec0bd5b718263ef6"})
+presenceOfUserFullAPIResponse = HTTParty.get('https://slack.com/api/users.getPresence', headers: {"Authorization" => "Bearer xoxp-2897666266758-2928076043648-2909522313811-2b89a3ceeb4ff4570a258f541accca2b"})
 
 presenceOfUser = presenceOfUserFullAPIResponse["online"]
 
@@ -12,5 +12,7 @@ else
     resultOfTheDeal = '課金します。自分で決めた約束は守ろう！勉強代だと思って払ってください。'
 end
 
+# Making a new variable so i can use it in result.html.erb
+@resultOfTheDeal = resultOfTheDeal
 
 
